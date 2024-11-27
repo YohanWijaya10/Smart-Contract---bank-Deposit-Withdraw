07a259b85a33d28c5ba3bf163767a551be0d8941
YohanWijaya10 <yohanwijaya10@gmail.com>
2024-11-27 11:17:44 +0700

    first

---
+++ contracts/3_Ballot.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bank {
+
+    mapping(address => uint ) public balance;
+
+    event topUpEvent(address indexed user, uint amount);
+    event withDrawEvent(address indexed user, uint amount);
+
+    function topUp() public payable {
+        require(msg.value > 0 ,"saldonya kurang boss qiu");
+        balance[msg.sender] += msg.value;
+        emit withDrawEvent(msg.sender, msg.value);
+
+    }
+
+    function withdraw(uint _Amount) public {
+        uint toEther = _Amount * 1 ether;
+        require(balance[msg.sender] >= toEther, "" );
+        balance[msg.sender] -= toEther ;
+        payable(msg.sender ).transfer(toEther);
+        emit withDrawEvent(msg.sender, toEther);
+
+    }
+
+    function smallWithdraw(uint _amount) public {
+        require(balance[msg.sender] >= _amount, "saldo Kurang bro");
+        balance[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+        emit withDrawEvent(msg.sender , _amount); 
+    }
+    
+}
